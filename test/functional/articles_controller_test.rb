d3630fbb2e73e47da60e1fbbeb778f7cae7d7224
Arugin <arugin@arugin.(none)>
2013-07-29 23:23:29 +0400

    remove reduntant views

--- test/functional/articles_controller_test.rb
+++ test/functional/articles_controller_test.rb
@@ -16,19 +16,4 @@ class ArticlesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
-  end
-
 end
