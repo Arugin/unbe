f79f62dcbaea6dbe224625193f7f573741ee2cc1
Arugin <arugin@arugin.(none)>
2013-08-17 20:41:25 +0400

    xinha engine

---
+++ public/xinha/plugins/SuperClean/lang/ru.js
@@ -0,0 +1,17 @@
+// I18N constants
+// LANG: "nl", ENCODING: UTF-8
+// Author: Maarten Molenschot, maarten@nrgmm.nl
+{
+  "Clean up HTML": "Очистить HTML",
+  "Please select from the following cleaning options...": "Пожалуйста, выберите из следующих опций ..",
+  "General tidy up and correction of some problems.": "Общие убирать и исправление некоторых проблем",
+  "Clean bad HTML from Microsoft Word": "Очистить плохой HTML из Microsoft Word",
+  "Remove custom typefaces (font \"styles\").": "Удаление пользовательских шрифтов",
+  "Remove custom font sizes.": "Удаление пользовательских размеров шрифта",
+  "Remove custom text colors.": "Удаление пользовательских цвет текста",
+  "Remove lang attributes.": "Удалить 'Lang' атрибуты.",
+  "Go": "Ок",
+  "Cancel": "Отменить",
+  "Tidy failed.  Check your HTML for syntax errors.": "Очистка не удаласть. Проверьте ваш HTML на синтаксичесике ошибки",
+  "You don't have anything to tidy!": "Все и так хорошо, зачем что-то очищать?"
+};
