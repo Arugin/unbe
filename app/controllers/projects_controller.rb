09fc38178ae6387e9b7d3d244b4d4ce47d433b90
Arugin <unbecinema@gmail.com>
2013-09-21 01:59:58 +0400

    Projects index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
   def index
-
+    @stats = Stats::Base.new
   end
 end
---
+++ app/models/stats/base.rb
@@ -0,0 +1,18 @@
+module Stats
+  class Base
+    include Mongoid::Document
+
+    def articles_count
+      Article.not_in(article_area: ArticleArea.where(title: "NEWS").first).and(isApproved: true).count
+    end
+
+    def users_count
+      User.all.count
+    end
+
+    def percentage(value, max_value)
+      (self.send("#{value.to_s}_count")/max_value.to_f)*100
+    end
+
+  end
+end
