5af81f064c124628523b7c0bde4352c190b7997b
Arugin <valera-sochi@bk.ru>
2014-02-23 00:08:33 +0400

    Tests step 2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,8 @@
 Unbe::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   Mongoid.identity_map_enabled = true
+  Mongoid.logger.level = Logger::ERROR
+  Moped.logger.level = Logger::ERROR
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
--- spec/controllers/contents_controller_spec.rb
+++ spec/controllers/contents_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe ContentsController do
+  def valid_session
+    {}
+  end
 
+  before(:each) do
+    @user = FactoryGirl.create(:user, gender: @male)
+  end
+
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "should not show not approved content" do
+      content = FactoryGirl.create(:content_base_content, author: @user)
+      get :index, {}
+
+      assigns(:contents).should eq []
+    end
+
+    it "should show approved content" do
+      content = FactoryGirl.create(:content_base_content, author: @user, approved_to_news: true, reviewed: true)
+      get :index, {}
+
+      assigns(:contents).should eq [content]
+    end
+  end
 end
