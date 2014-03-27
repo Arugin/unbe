cd3bc19e558e022591f8d782fcb09304b659f0f6
mayatskiy <unbecinema@gmail.com>
2014-03-27 19:37:38 +0400

    More tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = FactoryGirl.create(:user, gender: @male)
+  end
+
+  it "should have USER role by default" do
+    @user.highest_role.should eq 'USER'
+  end
+
+  it "should change to correct role" do
+    @user.change_role :ADMIN
+    @user.highest_role.should eq 'ADMIN'
+  end
 end
