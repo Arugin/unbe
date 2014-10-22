4f1c7b3166a45a62cd00f1732e3c15e4ec4b24e1
mayatskiy <unbecinema@gmail.com>
2014-10-22 16:03:31 +0400

    Modify whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,11 @@
-every :friday, :at => '8pm' do
+set :output, '/home/arugin/webapps/unbe/file.log'
+
+job_type :rake, "cd :path && :environment_variable=:environment GEM_HOME=$HOME/webapps/unbe/gems RUBY_LIB=$HOME/webapps/unbe/lib PATH=$HOME/webapps/unbe/bin:$PATH rake :task --silent :output"
+
+every :friday, at: '8pm' do
   rake  "mail:last_articles"
 end
 
-every :day, :at => '5am' do
+every :day, at: '5am' do
   rake  "update_ranks"
 end
