0190f05c9eee9dd09b1b07c3575c0add60f6fc13
mayatskiy <mayatskiy@actimind.com>
2015-08-05 14:11:58 +0400

    Enabling caching on amazon s3

--- app/models/image.rb
+++ app/models/image.rb
@@ -13,12 +13,13 @@ class Image
                                 access_key_id: ENV['AWS_ACCESS_KEY_ID'],
                                 secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
                             },
+                            s3_headers: { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate },
                             styles: {
                                 original: ['1920>', :jpg],
                                 medium:   ['650>',   :jpg],
                                 thumb:    ['212>',    :jpg]
                             },
-                            convert_options: { all: '-background white -flatten +matte' }
+                            convert_options: { all: '-quality 80 -background white -flatten +matte' }
 
   validates_attachment_size :file, less_than: 10000.kilobytes, message: I18n.t(:FILE_SIZE_IS_LIMITED_ARTICLE)
   validates_attachment_content_type :file, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, message: I18n.t(:ONLY_IMAGES_CAN_BE_UPLOADED)
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,22 +5,23 @@ class Picture
   embedded_in :imageable, polymorphic: true
 
   has_mongoid_attached_file :file,
-                            :path           => '/:class/:attachment/:id_partition/:style/:filename',
-                            :storage        => :s3,
-                            :url            => ':s3_domain_url',
-                            :s3_credentials => {
-                                :bucket => ENV['AWS_BUCKET'],
-                                :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                                :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                            path: '/:class/:attachment/:id_partition/:style/:filename',
+                            storage: :s3,
+                            url: ':s3_domain_url',
+                            s3_credentials: {
+                                bucket: ENV['AWS_BUCKET'],
+                                access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
                             },
-                            :styles => {
-                                :original => ['1920x1680>', :jpg],
-                                :small    => ['100x100>',   :jpg],
-                                :medium   => ['250x250',    :jpg],
-                                :large    => ['500x500>',   :jpg],
-                                :mini     => ['50x50>',   :jpg],
+                            s3_headers: { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate },
+                            styles: {
+                                original: ['1920x1680>', :jpg],
+                                small: ['100x100>',   :jpg],
+                                medium:['250x250',    :jpg],
+                                large: ['500x500>',   :jpg],
+                                mini:  ['50x50>',   :jpg],
                             },
-                            :convert_options => { :all => '-background white -flatten +matte' }
+                            :convert_options => { :all => '-quality 80 -background white -flatten +matte' }
 
   validates_attachment_size :file, less_than: 200.kilobytes, message: I18n.t(:FILE_SIZE_IS_LIMITED)
   validates_attachment_content_type :file, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png)$/, message: I18n.t(:ONLY_IMAGES_CAN_BE_UPLOADED)
